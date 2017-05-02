c27cbc7a4d3b45684590c5f1a8c4b1024d87931d
杨君华 <yangjh@gegejia.com>
2017-05-02 15:05:40 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = function(timestamp) {
+	if (typeof timestamp !== 'number' || isNaN(timestamp) || timestamp <= 0) {
+		return {
+			days: 0,
+			hours: 0,
+			minutes: 0,
+			seconds: 0,
+			milliseconds: 0
+		};
+	}
+
+	var days = Math.floor(timestamp / (24 * 60 * 60 * 1000));
+	var hours = Math.floor(timestamp % (24 * 60 * 60 * 1000) / (60 * 60 * 1000));
+	var minutes = Math.floor(timestamp % (60 * 60 * 1000) / (60 * 1000));
+	var seconds = Math.floor(timestamp % (60 * 1000) / 1000);
+	var milliseconds = Math.floor(timestamp % 1000);
+
+	return {
+		days: d,
+		hours: hours,
+		minutes: minutes,
+		seconds: seconds,
+		milliseconds: milliseconds
+	}
+}
